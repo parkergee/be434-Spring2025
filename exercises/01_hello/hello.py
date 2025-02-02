5ca17cd906cab1ba50ebaf540a04d85c9221d972
parkergee <parkergeffre@email.arizona.edu>
2025-02-02 16:57:49 -0700

    Complete Exercise 1

--- exercises/01_hello/hello.py
+++ exercises/01_hello/hello.py
@@ -1,1 +1,26 @@
-print("Hewwo, World!")
+#!/c/Users/parkergeffre/AppData/Local/Microsoft/WindowsApps/python3
+
+"""
+Author: Parker Geffre
+Purpose: Say hello
+"""
+
+import argparse
+
+
+def get_args():
+    """get command line args"""
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n','--name', metavar = "name", default = 'World', help='Name to greet')
+    return parser.parse_args()
+
+
+def main(): 
+    """main"""
+    args = get_args()
+    name = args.name
+    print("Hello, "+name + "!")
+
+
+if __name__ == '__main__':
+    main()
